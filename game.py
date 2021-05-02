1e89d193c18d426339bfd122293c9222190fea15
malloryeastburn <mallorykaitlin@gmail.com>
2021-05-01 23:05:27 -0500

    Updates to user inputted board option

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 #Default board
 board = [
-    [7,5,0,4,0,0,1,2,0],
+    [7,8,0,4,0,0,1,2,0],
     [6,0,0,0,7,5,0,0,9],
     [0,0,0,6,0,1,0,7,8],
     [0,0,7,0,4,0,2,6,0],
@@ -11,6 +11,21 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 #Add in option for user to input they're own puzzle
+#nswer = input("Do you want to use your own puzzle? (y,n): ")
+#f answer == "y":
+#   for i in range(len(board)):
+#       nums = input("Enter the numbers for row " + str(i+1) + " seperated by a comma.\nUse zero for blank spaces. (1,0,3,0,0,6,0,0,9): ")
+#       numsArr =  nums.split(",")
+#       while len(numsArr) != len(board[0]):
+#           print("Seems like you inputted something incorrectly, try again.")
+#           nums = input("Enter the numbers for row " + str(i+1) + " seperated by a comma.\nUse zero for blank spaces. (1,0,3,0,0,6,0,0,9): ")
+#           numsArr =  nums.split(",")
+#       for j in range(len(board[0])):
+#           while int(numsArr[j]) > 9 or int(numsArr[j]) < 0:
+#               print("Seems like you inputted something incorrectly, try again.")
+#               num = input("What number did you want in row " + str(i+1) + " and column + " + str(j+1) + "? ")
+#               numsArr[j] = num
+#           board[i][j] = int(numsArr[j].strip())
 
 #Backtracking function that solves the board. Calls itself when a solution is invalid until the board is solved
 def solve(board):
